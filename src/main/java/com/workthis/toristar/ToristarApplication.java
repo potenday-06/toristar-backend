9d9bf4d6dd47ea84501116feb071efd781b9a259
logan-mac <ralla0405@gmail.com>
2025-02-23 00:16:57 +0900

    first commit

---
+++ src/main/java/com/workthis/toristar/ToristarApplication.java
@@ -0,0 +1,13 @@
+package com.workthis.toristar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToristarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToristarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workthis/toristar/ToristarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workthis.toristar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToristarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
