0ce2064730b50ca30587659dfb02b5f0974fef46
logan-mac <ralla0405@gmail.com>
2025-02-27 15:28:57 +0900

    refact: 로그인시 쿠키 설정

--- src/main/java/com/workthis/toristar/auth/ui/AuthRestController.java
+++ src/main/java/com/workthis/toristar/auth/ui/AuthRestController.java
@@ -6,7 +6,9 @@ import com.workthis.toristar.member.domain.Provider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "인증 APIs")
@@ -22,9 +24,18 @@ public class AuthRestController {
     @GetMapping("/provider/{provider}")
     public AuthResponse registerOrLogin(
             @Schema(description = "카카오: kakao, 네이버: naver") @PathVariable(name = "provider") String provider,
-            @Schema(description = "각 플랫폼에서 받은 인가코드") @RequestParam(name = "code") String code
+            @Schema(description = "각 플랫폼에서 받은 인가코드") @RequestParam(name = "code") String code,
+            HttpServletResponse response
     ) {
-        return registerOrLoginUseCase.execute(Provider.fromString(provider), code);
+        AuthResponse authResponse = registerOrLoginUseCase.execute(Provider.fromString(provider), code);
+        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", authResponse.accessToken()) // 토큰 값 설정
+                .httpOnly(true)  // JavaScript에서 접근 불가능
+                .secure(true)    // HTTPS 환경에서만 전송
+                .sameSite("Strict") // CSRF 방어
+                .path("/")       // 모든 경로에서 쿠키 적용
+                .build();
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+        return authResponse;
     }
 
 }
